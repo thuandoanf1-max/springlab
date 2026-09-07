72b72b827c2f29bc65c2ec2e3c29d04c28864741
thuandoanf1-max <thuandoanf1@gmail.com>
2026-09-07 15:52:33 +0700

    Initial Spring Boot project

---
+++ src/test/java/com/hcmute/springlab/SpringlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmute.springlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
